f6994329e274b76a9b2cb63c613311bf2cc40056
Juan Pedro Michelini Lopez <juanpemichelini@gmail.com>
2025-09-10 14:57:32 -0300

    arreglo respuesta config pipeline

--- src/controllers/pipelineController.ts
+++ src/controllers/pipelineController.ts
@@ -3,7 +3,17 @@ import { PipelineConfig } from '../pipelines/orderPipeline';
 
 // Configuración en memoria para el pipeline
 let pipelineConfig: PipelineConfig = {
-    enabledFilters: [] // Se puede inicializar con los nombres de los filtros activos
+    // Inicializar con todos los filtros registrados en el pipeline
+    enabledFilters: [
+        'CustomerValidationFilter',
+        'ProductValidationFilter',
+        'DataIntegrityFilter',
+        'PriceCalculationFilter',
+        'MembershipDiscountFilter',
+        'VolumeDiscountFilter',
+        'TaxCalculationFilter',
+        'PaymentProcessingFilter'
+    ]
 };
 
 export const getPipelineConfig = (req: Request, res: Response) => {
